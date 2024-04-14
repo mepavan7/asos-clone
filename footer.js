fe3b22d780d97da20b3bc36f126968d27492e3fe
PavanAnnaparthi21 <113789537+PavanAnnaparthi21@users.noreply.github.com>
2024-04-14 22:43:11 +0530

    asos website clone

---
+++ footer.js
@@ -0,0 +1,40 @@
+function footer(){
+    return` <div id="footer">
+    <div id="icons1">
+        <img src="signin_png/facebook.png" alt="facebook" height="30px">
+        <img src="signin_png/google.png" alt="google" height="30px">
+        <img src="signin_png/twitter.png" alt="twitter" height="30px">
+    </div>
+    <div id="all_details1">
+        <div id="details1">
+            <h3>HELP & INFORMATION</h3>
+            <p>Help</p>
+            <p>Track order</p>
+            <p>Delivery & returns</p>
+        </div>
+        <div id="details1">
+            <h3>ABOUT ASOS</h3>
+            <p>About us</p>
+            <p>Careers at ASOS</p>
+            <p>Corporate responsibility</p>
+            <p>Invester's site</p>
+        </div>
+        <div id="details1">
+            <h3>MORE FROM ASOS</h3>
+            <p>Mobile & ASOS apps</p>
+            <p>ASOS Marketplace</p>
+            <p>Gift vouchers</p>
+            <p>Black Friday</p>
+        </div>
+        <div id="details1">
+            <h3>SHOPPING FROM:</h3>
+            <P>You are in INDIA</P>
+        </div>
+    </div>
+    <div id="about1">
+       <p id="year">2024 ASOS</p>
+       <p id="policy">Privacy & Cookies | Ts&Cs | Accesbility</p>
+    </div>
+</div>`
+}
+export{footer}
---
+++ navbar.js
@@ -0,0 +1,29 @@
+function navbar (){
+    return` <div class="top">
+    <div class="topbar">
+        <ul class="list">
+            <li>
+                <a class="para">| Marketplace  |
+                </a>
+            </li>
+            <li>
+                <a class="para">
+                 Help & FAQs   |
+                </a>
+                </li>
+            <img src="	https://assets.asosservices.com/storesa/images/flags/in.png" |
+                class="flag"></img>|
+            </li>
+        </ul>
+    </div>
+</div>
+<div id="down">
+    <div id="logo"> <img src="https://1000logos.net/wp-content/uploads/2020/07/Asos-Logo.jpg" alt="asos_logo" width="60%"></div>
+    <h2 class="women">WOMEN</h2>
+    <a href = "adidas.html"><h2 class="men">MEN</h2></a>
+    <input type="search" id="search">
+    <a href="login.html"><img src="signin_png/login.png" alt="login" class="login"></a>
+    <a href = "cart.html"><img src="signin_png/icons8-cart-64.png" alt="cart" class="login"></a>
+</div>`
+}
+export {navbar}
